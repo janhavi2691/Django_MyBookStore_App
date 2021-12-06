96f5e805e43a7ec8ad31a3148ef991348a8c12c0
Janhavi Borse <81101239+janhavi2691@users.noreply.github.com>
2021-12-06 12:38:25 +0530

    Update views.py
    unused function deleted

--- MyBookStore/views.py
+++ MyBookStore/views.py
@@ -40,22 +40,3 @@ class search_book_by_author(ListView):
 
 
 
-'''
-def search_book(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = SearchForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/searchbook/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = SearchForm()
-
-    return render(request, 'home.html', {'form': form})
-'''
